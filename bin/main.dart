202c89910e2bf49fbc4b8b21083229808960c630
debjohnson33 <debjohnson_11@yahoo.com>
2019-05-29 19:34:58 -0500

    Updated packages/added second function to project

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,8 @@
 import 'package:path/path.dart';
 import 'package:mock_data/mock_data.dart' as mock_data;
+import 'package:dart_project/awesome_package.dart';
 
 void main() {
   print(mock_data.mockString());
+  print(getCurrentTimestamp());
 }
